678367ec99128fe2cbf0089039fd5ad4279a1530
arfat-beep <arfatrahman08@gmail.com>
2022-04-28 15:37:04 +0600

    a try

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4500;
 const app = express();
 
 // middleware
