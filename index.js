ab039c416ee7fe3a267a09934495064b2061ed5d
arfat-beep <arfatrahman08@gmail.com>
2022-04-28 22:00:12 +0600

    trying

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ const run = async () => {
     app.post("/productByKeys", async (req, res) => {
       const keys = req.body;
       const ids = keys.map((id) => ObjectId(id));
-      console.log(keys);
       const query = { _id: { $in: ids } };
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
